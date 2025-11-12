a10d945a66f78376eeac37429802008b1b1101e2
/stambol <129373365+stambolce@users.noreply.github.com>
2025-11-12 09:21:45 +0100

    Change 'Powered by ByteStash' to 'Powered by Stambol'

--- client/src/components/snippets/embed/EmbedModal.tsx
+++ client/src/components/snippets/embed/EmbedModal.tsx
@@ -110,7 +110,7 @@ export const EmbedModal: React.FC<EmbedModalProps> = ({
                 checked={showPoweredBy}
                 onChange={setShowPoweredBy}
               />
-              <span>Show "Powered by ByteStash"</span>
+              <span>Show "Powered by Stambol"</span>
             </label>
 
             <div>
